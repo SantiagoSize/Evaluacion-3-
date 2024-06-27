fa84ed143445c78bf28e6a789c1b20e3d2cf1598
LucassFigueroa <luc.figueroam@duocuc.cl>
2024-06-27 00:06:22 -0400

    Update Programa Principal.py

---
+++ Programa Principal.py
@@ -0,0 +1,9 @@
+print("===============================================")
+print("Bienvenido al terminal de arriendo de viviendas")
+print("===============================================")
+
+
+
+from Libreria import (solicitar_RUT, crear_contraseña, mostrar_reglas, solicitar_informacion_arriendo,
+    solicitar_liquidaciones, mostrar_informacion_arriendo, solicitar_continuacion,menu_arriendos, especificaciones_arriendo,
+      mostrar_informacion_vivienda,agregar_vivienda_arrendada, agregar_vivienda_publicada, ver_viviendas)
